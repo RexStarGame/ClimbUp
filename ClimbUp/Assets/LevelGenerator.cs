e7ab8a0bca8c2be1d988dffae41b11cf3d3f2c00
Patrick <63421519+RexStarGame@users.noreply.github.com>
2025-01-29 14:24:55 +0100

    Update

--- ClimbUp/Assets/LevelGenerator.cs
+++ ClimbUp/Assets/LevelGenerator.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject floors;
+    private List<Vector2> floorsList = new();
+    private void flip(GameObject go, Transform tra)
     {
-        
+        go.transform.rotation = new Quaternion(go.transform.rotation.x, tra.rotation.y + 180, go.transform.rotation.z, go.transform.rotation.w);
     }
 
-    // Update is called once per frame
-    void Update()
+    internal void SpawnNewLevel(Transform tra)
     {
-        
+        if(floorsList.Contains(tra.position))
+        {
+            return;
+        }
+        GameObject go = Instantiate(floors, new Vector2(0, tra.position.y + 10.06f), Quaternion.identity);
+        if(go.TryGetComponent(out LevelController levelController))
+        {
+            levelController.levelGenerator = this;
+        }
+        floorsList.Add(tra.transform.position);
+        flip(go, tra);
     }
 }
