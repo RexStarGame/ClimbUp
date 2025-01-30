b418784f0ffc05e31eb601d13de975fd638352fb
Patrick <63421519+RexStarGame@users.noreply.github.com>
2025-01-30 10:37:21 +0100

    update

--- ClimbUp/Assets/LavaController.cs
+++ ClimbUp/Assets/LavaController.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class LavaController : MonoBehaviour
 {
-    [SerializeField] private GameObject lava;
+    [SerializeField] private float moveSpeed = 2f; 
+    [SerializeField] private float startDelay = 10f; 
+    private Rigidbody2D rb2D;
+    private bool isRising = false; 
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(StartLavaRise()); 
+    }
+
+    private IEnumerator StartLavaRise()
+    {
+        yield return new WaitForSeconds(startDelay); 
+        isRising = true; 
+    }
+
+    private void Update()
+    {
+        if (isRising)
+        {
+            rb2D.velocity = new Vector2(0f, moveSpeed); 
+        }
+        else
+        {
+            rb2D.velocity = Vector2.zero; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject); // player dies. 
-
+            Destroy(other.gameObject);
+            Time.timeScale = 0; 
         }
     }
 }
